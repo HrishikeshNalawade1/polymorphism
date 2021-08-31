5ffd39b0dbe434bf48e86103a29e5ea59c8f757e
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-08-31 11:03:13 +0530

    Add files via upload

---
+++ polymorphism_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace polymorphism_1
+{
+    public class addition
+    {
+        public void add()
+        {
+            int a = 30;
+            int b = 20;
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+
+        public void add(int a, int b)
+        {
+            int c = a + b;
+            Console.WriteLine("\n"+c);
+        }
+        public void add(string a, string b)
+        {
+            string c =a+" "+b ;
+            Console.WriteLine("\n"+c);
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                addition a = new addition();
+                a.add();
+                a.add("hii","hello");
+               a.add(9, 9);
+            }
+        }
+    }
+
